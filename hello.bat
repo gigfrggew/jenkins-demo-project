9238fef3ae6ee1b921c68250e21410212407bd54
Bharath B Shetty <158118484+gigfrggew@users.noreply.github.com>
2026-03-13 07:49:03 +0530

    Fix typo in webhook trigger success message

--- hello.bat
+++ hello.bat
@@ -2,3 +2,5 @@ echo hello from github repository
 echo jenkins succesfully pulled this project
 echo webhook trigger configuration setted
 echo webook trigger succesfully built
+
+echo webohhok error fixed trigger succesffuly built#444
